5310536f4a61e71364b673d42cea73adbb112241
Seth D Joslin <seth.joslin@yahoo.com>
2024-09-12 11:55:30 -0500

    added form that collects user info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 // for index html
 
-console.log("project-0");
+const form = document.getElementById("userinfo");
+
+let consumerData = [];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // prevent default form submission
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  //Do something with the form data, e.g., send it to server
+  console.log("Name:", name);
+  console.log("Email:", email);
+});
